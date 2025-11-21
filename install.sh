0a6b88923e96bdfd86bb30b0b313d23b8a77a49d
seal <sealonthemoon.online@gmail.com>
2025-11-22 00:33:10 +0100

    Enhance install.sh output clarity by adding prefixes to echo statements for better user guidance during installation steps.

--- install.sh
+++ install.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 
 echo "---------------------------------------------"
-echo "Setting directories..."
+echo "==> Setting directories..."
 DIR=$1
 REPO="$DIR/wsl-dl-instance"
-echo "Done!"
 
-echo "Installing packages..."
+echo "==> Installing packages..."
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y git nano wget ffmpeg pip python3
-echo "Done!"
 
-echo "Installing youtube-dl..."
+echo "==> Installing youtube-dl..."
 sudo cp "$REPO/youtube-dl" /usr/local/bin/
 sudo chmod a+rx /usr/local/bin/youtube-dl
 alias youtube-dl='python3 /usr/local/bin/youtube-dl'
 alias ydl='python3 /usr/local/bin/youtube-dl'
-echo "Done!"
 
-echo "Moving files to $DIR..."
+echo "==> Moving files to $DIR..."
 sudo cp -r "$REPO"/* "$DIR"
-echo "Done!"
 
-echo "Cleaning up..."
+echo "==> Cleaning up..."
 sudo rm -rf "$REPO"
 sudo rm -r "$DIR/youtube-dl"
+sudo mkdir -p "$DIR/audio"
 cd "$DIR" | ls
-echo "Done!"
+echo "==> Done!"
+echo "---------------------------------------------"
 
-echo "Updating paths in scripts..."
+echo "==> Updating paths in scripts..."
 sudo chmod +x "$DIR"/*.sh
 sudo "$DIR/setup.sh"
-echo "Done!"
+echo "==> Done!"
+echo "---------------------------------------------"
 
-echo "Making user owner..."
+echo "==> Making user owner..."
 sudo chmod +x "$DIR"/*.sh
 sudo "$DIR/owner.sh"
 echo "Done!"
 
 echo "---------------------------------------------"
 echo "==> youtube-dl has successfully been installed"
-echo "=> run 'youtube-dl --help' for more info"
-echo "=> or run 'python3 /usr/local/bin/youtube-dl --help' if the above does not work"
+echo "=> run 'python3 /usr/local/bin/youtube-dl --help' for more info"
+echo "=> run 'sudo alias youtube-dl='python3 /usr/local/bin/youtube-dl'' to set an alias"
